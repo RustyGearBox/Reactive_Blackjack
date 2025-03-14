fa5e6975de1586bc3311d99827686a13ca15a96a
RustyGearBox <gearbox.bit@gmail.com>
2025-03-14 23:54:59 +0100

    GameRepository annotation

--- src/main/java/edu/blackjack/repositories/GameRepository.java
+++ src/main/java/edu/blackjack/repositories/GameRepository.java
@@ -1,10 +1,12 @@
 package edu.blackjack.repositories;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 import edu.blackjack.models.Game;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface GameRepository extends ReactiveMongoRepository<Game, String> {
     Mono<Game> findByGameId(String id);
 }
